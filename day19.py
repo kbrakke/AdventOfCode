54d9b824301279710cc4718f63eddf5eee2a5574
kbrakke <kbrakke@gmail.com>
2017-12-24 18:23:18 -0700

    Initial thoughts and input

---
+++ day19.py
@@ -0,0 +1,17 @@
+grid = []
+
+for line in open('day19Input.txt'):
+    grid.append(list(line))
+
+
+direction = 'down'
+current_char = '|'
+current_position = ("".join(grid[0]).find('|') , 0) # x, y
+letters_reached = []
+
+def next_position():
+    if direction == 'down':
+
+
+while True:
+
