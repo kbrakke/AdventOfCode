24d0fb0b390b99c61ec9fb765bb3148d123d01ab
kbrakke <kbrakke@gmail.com>
2018-12-02 09:56:13 -0500

    Very gross day 1

---
+++ AOC-2018/day1.go
@@ -0,0 +1,43 @@
+package main
+
+import(
+	"bufio" 
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Printf("Advent of Code Day 1\n")
+	file, _ := os.Open("day1Input.txt")
+	frequencyChanges := make([]int, 0)
+	var matchedFrequency bool = false 
+	reachedFrequencies := make(map[int]int)
+	sum := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineInt, _ := strconv.Atoi(scanner.Text())
+		frequencyChanges = append(frequencyChanges, lineInt)
+		sum = sum + lineInt
+		if (reachedFrequencies[sum] > 0) {
+			matchedFrequency = true
+			fmt.Printf("Matched Frequency %d\n", sum)
+		} else {
+			reachedFrequencies[sum] = 1
+		}
+	}
+	fmt.Printf("Final frequency %d\n", sum)
+	fmt.Printf("Matched Freq %v\n", matchedFrequency)
+	for !matchedFrequency {
+		for _, value := range frequencyChanges {
+			sum = sum + value
+			if (reachedFrequencies[sum] == 1) {
+				matchedFrequency = true
+				fmt.Printf("Matched Frequency %d\n", sum)
+				break
+			} else {
+				reachedFrequencies[sum] = 1
+			}
+		}
+	}
+}
---
+++ AOC-2018/readme.md
@@ -0,0 +1,6 @@
+# Advent of Code 2018
+
+This year I wanted to try GO, so here we are
+
+[x] Day 1
+[] Day 2
