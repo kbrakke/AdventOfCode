569a9709214f7199c335d5d4670b9f70c559fa1c
kbrakke <kbrakke@gmail.com>
2018-12-10 00:10:18 -0500

    A reasonably satisfying day 3

---
+++ AOC-2018/src/adventOfCode/day3.go
@@ -0,0 +1,58 @@
+package adventOfCode
+
+import(
+	"bufio" 
+	"fmt"
+	"os"
+	"strconv"
+	"regexp"
+)
+
+func Day3(filename string) {
+	fmt.Printf("Advent of Code Day 3\n")
+	file, _ := os.Open(filename)
+	scanner := bufio.NewScanner(file)
+	fabric := make([][]int, 1000)
+	for i := range fabric {
+		fabric[i] = make([]int, 1000)
+	}
+	claimSize := 0
+	trueClaims := make(map[int]struct{}, 0)
+	exists := struct{}{}
+	for scanner.Scan() {
+		id, fromLeft, fromTop, width, height := parseLine(scanner.Text())
+		trueClaim := true
+		for row := 0; row < height; row++ {
+			for col := 0; col < width; col++ {
+				currentClaim := fabric[row + fromTop][col + fromLeft]
+				if currentClaim == 0 {
+					fabric[row + fromTop][col + fromLeft] = id
+				} else if currentClaim > 0 {
+					trueClaim = false;
+					delete(trueClaims, currentClaim)
+					fabric[row + fromTop][col + fromLeft] = -1
+					claimSize++
+				} else {
+					trueClaim = false;
+				}
+			}
+		}
+		if trueClaim {
+			trueClaims[id] = exists
+		}
+	}
+	
+	fmt.Printf("Claim size overall: %d\n", claimSize)
+	fmt.Printf("The true claim %v\n", trueClaims)
+}
+
+func parseLine(line string) (int, int, int, int, int) {
+	lineRegex := regexp.MustCompile("[\\d]+") 
+	entries := lineRegex.FindAllString(line, -1)
+	id, _ := strconv.Atoi(entries[0])
+	fromLeft, _ := strconv.Atoi(entries[1])
+	fromTop, _ := strconv.Atoi(entries[2])
+	width, _ := strconv.Atoi(entries[3])
+	height, _ := strconv.Atoi(entries[4])
+	return id, fromLeft, fromTop, width, height
+}
--- AOC-2018/src/adventOfCodeMain.go
+++ AOC-2018/src/adventOfCodeMain.go
@@ -12,8 +12,14 @@ func main() {
 	adventOfCode.Day1("./input/day1.txt")
 	day1Total := time.Since(day1Start)
 	fmt.Printf("Day 1 Took %s\n", day1Total)
+	
 	day2Start := time.Now()
 	adventOfCode.Day2("./input/day2.txt")
 	day2Total := time.Since(day2Start)
 	fmt.Printf("Day 2 Took %s\n", day2Total)
+	
+	day3Start := time.Now()
+	adventOfCode.Day3("./input/day3.txt")
+	day3Total := time.Since(day3Start)
+	fmt.Printf("Day 3 Took %s\n", day3Total)
 }
