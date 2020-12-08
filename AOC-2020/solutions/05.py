4520b1ee821f67186742d125fc1384fe61e25ca6
Kris Brakke <kbrakke@sevone.com>
2020-12-08 14:26:04 -0500

    Also add files

---
+++ AOC-2020/solutions/05.py
@@ -0,0 +1,49 @@
+print("Advent of code day 5")
+
+
+def parsePass(boardingPass):
+    rowString = boardingPass[:7]
+    columnString = boardingPass[7:]
+    rowRange = range(128)
+    columnRange = range(8)
+    row = 0
+    column = 0
+    for char in list(rowString):
+        if char == 'F':
+            rowRange = rowRange[:int(len(rowRange)/2)]
+        elif char == 'B':
+            rowRange = rowRange[int(len(rowRange)/2):]
+        else:
+            print('Unknown {char} in row string')
+    row = rowRange[0]
+    for char in list(columnString):
+        if char == 'L':
+            columnRange = columnRange[:int(len(columnRange)/2)]
+        elif char == 'R':
+            columnRange = columnRange[int(len(columnRange)/2):]
+        else:
+            print('Unknown {char} in column string')
+    column = columnRange[0]
+    return (row, column)
+
+
+maxPass = 0
+takenSeats = []
+allSeats = []
+for row in range(1, 127):
+    for col in range(8):
+        allSeats.append(row*8 + col)
+
+for line in open('/home/kbrakke/personal/AdventOfCode/AOC-2020/input/05/05-input.txt'):
+    (row, col) = parsePass(line.rstrip())
+    num = row * 8 + col
+    takenSeats.append(num)
+    if num > maxPass:
+        maxPass = num
+
+for seat in allSeats:
+    if seat not in takenSeats:
+        if seat + 1 in takenSeats and seat-1 in takenSeats:
+            print("Possible Seat: ", seat)
+
+print("Seat ID: ", maxPass)
