7e7ca6ab69b4898bebe3af44e6af75514c892e1d
Kris Brakke <kbrakke@gmail.com>
2022-12-25 11:42:27 -0500

    fix(pages): add proper base to handle repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kbrakke.github.io',
+  base: 'AdventOfCode',
   output: "server",
   adapter: node({
     mode: 'standalone'
