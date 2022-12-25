0c8602036b9d5ff637d354fddbdd1919e602fd7d
Kris Brakke <kbrakke@kriss-air.mynetworksettings.com>
2022-12-25 11:31:20 -0500

    fix(pages): update basepath and domain for git pages

--- AOC-2022/astro.config.mjs
+++ AOC-2022/astro.config.mjs
@@ -4,7 +4,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kbrakke.github.io',
-  base: '/AdventOfCode',
+  base: '/AdventOfCode/AOC-2022',
   output: "server",
   adapter: node({
     mode: 'standalone'
