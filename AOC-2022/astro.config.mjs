2971d259228841969279f16f426508087b7a0048
Kris Brakke <kbrakke@kriss-air.mynetworksettings.com>
2022-12-25 11:20:24 -0500

    feat(day 1): updated the UI for day 1 to be slightly nicer

--- AOC-2022/astro.config.mjs
+++ AOC-2022/astro.config.mjs
@@ -3,6 +3,8 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kbrakke.github.io',
+  base: '/AdventOfCode',
   output: "server",
   adapter: node({
     mode: 'standalone'
--- AOC-2022/src/components/inputReader.ts
+++ AOC-2022/src/components/inputReader.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs/promises';
 
-export async function readFile(path: string): Promise<string[]> {
-
+export async function readFile(path: string): Promise<string> {
+    return fs.readFile(`public/${path}`, { encoding: 'utf8' })
 }
